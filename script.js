c876f0d01c6d797f5cf0226f92ec7d2b0217ff9f
Mahmud Islam <109871308+mahmudislam47@users.noreply.github.com>
2022-10-26 21:51:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+    }
+  
     clear() {
       this.currentOperand = ''
       this.previousOperand = ''
